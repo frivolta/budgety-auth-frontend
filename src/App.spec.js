2569a83afdd43f6d91e690d25c073445d44b2ae6
frivolta <rivoltafilippo@gmail.com>
2020-06-08 00:53:14 +0200

    Merging apollo connection

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,8 +1,4 @@
-import React from 'react'
-import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
-
-import App from './App'
 
 describe('App', () => {
   it('renders without error', () => {
