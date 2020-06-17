a63cbc8da34c4997d6f4cdb429801199a3256fff
frivolta <rivoltafilippo@gmail.com>
2020-06-18 00:20:45 +0200

    Cypress stub requests

--- cypress/integration/signupValidation.tsx
+++ cypress/integration/signupValidation.tsx
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
-import { testUser } from '../../support/users'
-import { NETWORK } from '../../support/variables'
-import { SIGNUP_ERRORS } from '../../../src/utils/messages'
-import { SIGNUP } from '../../../src/pages/SignupPage'
+import { testUser } from '../support/users'
+import { NETWORK } from '../support/variables'
+import { SIGNUP_ERRORS } from '../../src/utils/messages'
+import { SIGNUP } from '../../src/pages/SignupPage'
 /**
  *  1) User get validation visual errors
  *  - Type username
@@ -14,9 +14,26 @@ import { SIGNUP } from '../../../src/pages/SignupPage'
  *  - Throws an error if required fileds are empty
  *  - Shows enabled button if all fields are correctly filled
  */
+
+const failResp = {
+  data: null,
+  errors: [
+    {
+      message: 'na',
+      locations: [{ line: 2, column: 3 }],
+      path: ['signup'],
+      code: 3010,
+      requestId: 'local:ckbjwtmld00200739vgpfo3oe',
+    },
+  ],
+}
 describe('Signup', function () {
   this.beforeEach(() => {
-    cy.visit(`${NETWORK.LOCAL}/signup`).contains('Sign up')
+    cy.visit(`${NETWORK.LOCAL}/signup`, {
+      onBeforeLoad: (win) => {
+        win.fetch = null
+      },
+    }).contains('Sign up')
   })
 
   it('correctly shows fields and disabled button', () => {
@@ -56,26 +73,33 @@ describe('Signup', function () {
 })
 
 describe('Signup requests', function () {
-  it('shows enabled button if fields are filled correctly ', async () => {
+  it('shows enabled button if fields are filled correctly ', () => {
+    cy.server()
+    cy.route({
+      url: 'https://auth-prisma-dev.herokuapp.com/',
+      method: 'POST',
+      status: 200,
+      response: failResp,
+      delay: 1000,
+    }).as('signup-reject')
     cy.get('input[name="email"]').click().clear().type(testUser.email)
     cy.get('input[name="password"]').click().clear().type(testUser.password)
     cy.get('input[name="confirmPassword"]')
       .click()
       .clear()
       .type(testUser.password)
-
-    // Stub rejection
-    cy.server()
-    cy.route({
-      url: 'https://auth-prisma-dev.herokuapp.com/',
-      method: 'POST',
-      status: 400,
-      response: { code: 400, message: 'Email already taken' },
-      delay: 1000,
-    }).as('signup-reject')
     cy.get('button').should('be.enabled').click()
+
     cy.wait('@signup-reject').then((xhr) => {
-      expect(xhr.status).to.equal(400)
+      console.log(xhr)
+      expect(xhr.status).to.equal(200)
     })
   })
 })
+
+// User signup request
+// User signup rejected
+// Can navigate to signin page
+// Is redirected if token already present
+// Intercept toaster
+// Same tests with rtl
